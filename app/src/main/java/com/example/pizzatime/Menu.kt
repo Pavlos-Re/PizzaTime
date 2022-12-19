d2f70f832c9e1cf4ae8b8f0bc6f55137e4e085a2
Pavlos Repin <pavlos.repin@gmail.com>
2022-12-20 01:53:46 +0200

    Buttons added

--- app/src/main/java/com/example/pizzatime/Menu.kt
+++ app/src/main/java/com/example/pizzatime/Menu.kt
@@ -1,28 +1,29 @@
 package com.example.pizzatime
+
 import android.os.Bundle
-import android.print.PrintAttributes.Margins
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
-import androidx.compose.foundation.gestures.scrollable
-import androidx.compose.foundation.interaction.DragInteraction
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.modifier.modifierLocalConsumer
-import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.pizzatime.ui.theme.PizzaTimeTheme
+
+var money_sum : Double = 0.0
 
 class Menu : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,11 +36,33 @@ class Menu : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
-                        Text(text = "Menu",modifier = Modifier.padding(30.dp), fontWeight = FontWeight.Bold, fontSize = 40.sp, fontFamily = FontFamily.Cursive)
+                        Text(text = "Menu",modifier = Modifier.padding(20.dp), fontWeight = FontWeight.Bold, fontSize = 40.sp, fontFamily = FontFamily.Cursive)
 
+                        Spacer(modifier = Modifier.height(10.dp))
+
+                        Text("Main Dishes",fontWeight = FontWeight.Bold, fontSize = 30.sp, fontFamily = FontFamily.Cursive)
                         PizzaList()
 
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        Text("Drinks",fontWeight = FontWeight.Bold, fontSize = 30.sp, fontFamily = FontFamily.Cursive)
                         DrinksList()
+
+                        Spacer(modifier = Modifier.height(90.dp))
+
+                        Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+
+                            Button(onClick = { /*TODO*/ }) {
+                                Text(text = "Done")
+                            }
+
+                            Button(onClick = { /*TODO */ }) {
+                                Text(text= "List")
+                            }
+
+                        }
+
+                        Spacer(modifier = Modifier.height(40.dp))
 
                     }
 
@@ -50,17 +73,25 @@ class Menu : ComponentActivity() {
 }
 
 @Composable
-fun DrinksList() {
-    Column(Modifier.border(5.dp, Color.Black, RectangleShape)) {
-        Row() {
+fun ColumnScope.DrinksList() {
+
+    val context = LocalContext.current
+
+    Column(
+        Modifier
+            .border(5.dp, Color.Black, RectangleShape)
+            .verticalScroll(rememberScrollState())
+            .weight(1f, false)
+    ) {
+        Row(modifier = Modifier.height(intrinsicSize = IntrinsicSize.Max)) {
             Text(
-                text = "Cola Drink",
+                text = "Cola Drink 1$",
                 fontSize = 20.sp,
                 fontWeight = FontWeight.Bold,
                 modifier = Modifier.padding(10.dp)
             )
             Button(
-                onClick = { }
+                onClick = { money_sum += 1; Toast.makeText(context, "Drink Added!", Toast.LENGTH_SHORT).show() }
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.add_icon),
@@ -70,15 +101,15 @@ fun DrinksList() {
                 Text(text = "Add")
             }
         }
-        Row() {
+        Row(modifier = Modifier.height(intrinsicSize = IntrinsicSize.Max)) {
             Text(
-                text = "Orange Juice",
+                text = "Orange Juice 1$",
                 fontSize = 20.sp,
                 fontWeight = FontWeight.Bold,
                 modifier = Modifier.padding(10.dp)
             )
             Button(
-                onClick = { }
+                onClick = { money_sum += 1; Toast.makeText(context, "Drink Added!", Toast.LENGTH_SHORT).show() }
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.add_icon),
@@ -88,15 +119,15 @@ fun DrinksList() {
                 Text(text = "Add")
             }
         }
-        Row() {
+        Row(modifier = Modifier.height(intrinsicSize = IntrinsicSize.Max)) {
             Text(
-                text = "Lemonade Drink",
+                text = "Lemonade Drink 1$",
                 fontSize = 20.sp,
                 fontWeight = FontWeight.Bold,
                 modifier = Modifier.padding(10.dp)
             )
             Button(
-                onClick = { }
+                onClick = { money_sum += 1; Toast.makeText(context, "Drink Added!", Toast.LENGTH_SHORT).show() }
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.add_icon),
@@ -106,21 +137,101 @@ fun DrinksList() {
                 Text(text = "Add")
             }
         }
+        Row(modifier = Modifier.height(intrinsicSize = IntrinsicSize.Max)) {
+            Text(
+                text = "Sparkling Water 1$",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(10.dp)
+            )
+            Button(
+                onClick = { money_sum += 1; Toast.makeText(context, "Drink Added!", Toast.LENGTH_SHORT).show() }
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.add_icon),
+                    contentDescription = "Add item",
+                    Modifier.size(20.dp)
+                )
+                Text(text = "Add")
+            }
+        }
+        Row(modifier = Modifier.height(intrinsicSize = IntrinsicSize.Max)) {
+            Text(
+                text = "Water 1$",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(10.dp)
+            )
+            Button(
+                onClick = { money_sum += 1; Toast.makeText(context, "Drink Added!", Toast.LENGTH_SHORT).show() }
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.add_icon),
+                    contentDescription = "Add item",
+                    Modifier.size(20.dp)
+                )
+                Text(text = "Add")
+            }
+        }
+        Row(modifier = Modifier.height(intrinsicSize = IntrinsicSize.Max)) {
+            Text(
+                text = "Ice Tea 1$",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(10.dp)
+            )
+            Button(
+                onClick = { money_sum += 1; Toast.makeText(context, "Drink Added!", Toast.LENGTH_SHORT).show() }
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.add_icon),
+                    contentDescription = "Add item",
+                    Modifier.size(20.dp)
+                )
+                Text(text = "Add")
+            }
+        }
+        Row(modifier = Modifier.height(intrinsicSize = IntrinsicSize.Max)) {
+            Text(
+                text = "Beer 2$",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(10.dp)
+            )
+            Button(
+                onClick = { money_sum += 1; Toast.makeText(context, "Drink Added!", Toast.LENGTH_SHORT).show() }
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.add_icon),
+                    contentDescription = "Add item",
+                    Modifier.size(20.dp)
+                )
+                Text(text = "Add")
+            }
+        }
+
     }
 }
 
 @Composable
-fun PizzaList() {
-    Column(Modifier.border(5.dp, Color.Black, RectangleShape)) {
-        Row() {
+fun ColumnScope.PizzaList() {
+
+    val context = LocalContext.current
+
+    Column(
+        Modifier
+            .border(5.dp, Color.Black, RectangleShape)
+            .verticalScroll(rememberScrollState())
+            .weight(1f, false)) {
+        Row(modifier = Modifier.height(intrinsicSize = IntrinsicSize.Max)) {
             Text(
-                text = "Margarita Pizza",
+                text = "Margarita Pizza 5$",
                 fontSize = 20.sp,
                 fontWeight = FontWeight.Bold,
                 modifier = Modifier.padding(10.dp)
             )
             Button(
-                onClick = { }
+                onClick = { money_sum += 5; Toast.makeText(context, "Pizza Added!", Toast.LENGTH_SHORT).show() }
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.add_icon),
@@ -130,15 +241,15 @@ fun PizzaList() {
                 Text(text = "Add")
             }
         }
-        Row() {
+        Row(modifier = Modifier.height(intrinsicSize = IntrinsicSize.Max)) {
             Text(
-                text = "Pepperoni Pizza",
+                text = "Pepperoni Pizza 6$",
                 fontSize = 20.sp,
                 fontWeight = FontWeight.Bold,
                 modifier = Modifier.padding(10.dp)
             )
             Button(
-                onClick = { }
+                onClick = { money_sum += 6; Toast.makeText(context, "Pizza Added!", Toast.LENGTH_SHORT).show() }
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.add_icon),
@@ -148,15 +259,15 @@ fun PizzaList() {
                 Text(text = "Add")
             }
         }
-        Row() {
+        Row(modifier = Modifier.height(intrinsicSize = IntrinsicSize.Max)) {
             Text(
-                text = "Gustoza Pizza",
+                text = "Gustoza Pizza 6$",
                 fontSize = 20.sp,
                 fontWeight = FontWeight.Bold,
                 modifier = Modifier.padding(10.dp)
             )
             Button(
-                onClick = { }
+                onClick = { money_sum += 6; Toast.makeText(context, "Pizza Added!", Toast.LENGTH_SHORT).show() }
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.add_icon),
@@ -166,15 +277,15 @@ fun PizzaList() {
                 Text(text = "Add")
             }
         }
-        Row() {
+        Row(modifier = Modifier.height(intrinsicSize = IntrinsicSize.Max)) {
             Text(
-                text = "Hawaiian Pizza",
+                text = "Hawaiian Pizza 6.5$",
                 fontSize = 20.sp,
                 fontWeight = FontWeight.Bold,
                 modifier = Modifier.padding(10.dp)
             )
             Button(
-                onClick = { }
+                onClick = { money_sum += 6.5; Toast.makeText(context, "Pizza Added!", Toast.LENGTH_SHORT).show() }
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.add_icon),
@@ -184,15 +295,123 @@ fun PizzaList() {
                 Text(text = "Add")
             }
         }
-        Row() {
+        Row(modifier = Modifier.height(intrinsicSize = IntrinsicSize.Max)) {
             Text(
-                text = "BBQ Chicken Pizza",
+                text = "BBQ Chicken Pizza 7$",
                 fontSize = 20.sp,
                 fontWeight = FontWeight.Bold,
                 modifier = Modifier.padding(10.dp)
             )
             Button(
-                onClick = { }
+                onClick = { money_sum += 7; Toast.makeText(context, "Pizza Added!", Toast.LENGTH_SHORT).show() }
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.add_icon),
+                    contentDescription = "Add item",
+                    Modifier.size(20.dp)
+                )
+                Text(text = "Add")
+            }
+        }
+        Row(modifier = Modifier.height(intrinsicSize = IntrinsicSize.Max)) {
+            Text(
+                text = "Cheese Pizza 7$",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(10.dp)
+            )
+            Button(
+                onClick = { money_sum += 7; Toast.makeText(context, "Pizza Added!", Toast.LENGTH_SHORT).show() }
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.add_icon),
+                    contentDescription = "Add item",
+                    Modifier.size(20.dp)
+                )
+                Text(text = "Add")
+            }
+        }
+        Row(modifier = Modifier.height(intrinsicSize = IntrinsicSize.Max)) {
+            Text(
+                text = "Vegetable Pizza 5$",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(10.dp)
+            )
+            Button(
+                onClick = { money_sum += 5; Toast.makeText(context, "Pizza Added!", Toast.LENGTH_SHORT).show() }
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.add_icon),
+                    contentDescription = "Add item",
+                    Modifier.size(20.dp)
+                )
+                Text(text = "Add")
+            }
+        }
+        Row(modifier = Modifier.height(intrinsicSize = IntrinsicSize.Max)) {
+            Text(
+                text = "Vegan Pizza 5.5$",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(10.dp)
+            )
+            Button(
+                onClick = { money_sum += 5.5; Toast.makeText(context, "Pizza Added!", Toast.LENGTH_SHORT).show() }
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.add_icon),
+                    contentDescription = "Add item",
+                    Modifier.size(20.dp)
+                )
+                Text(text = "Add")
+            }
+        }
+        Row(modifier = Modifier.height(intrinsicSize = IntrinsicSize.Max)) {
+            Text(
+                text = "Buffalo Pizza 8$",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(10.dp)
+            )
+            Button(
+                onClick = { money_sum += 8; Toast.makeText(context, "Pizza Added!", Toast.LENGTH_SHORT).show() }
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.add_icon),
+                    contentDescription = "Add item",
+                    Modifier.size(20.dp)
+                )
+                Text(text = "Add")
+            }
+        }
+        Row(modifier = Modifier.height(intrinsicSize = IntrinsicSize.Max)) {
+            Text(
+                text = "Supreme Pizza 9$",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(10.dp)
+            )
+            Button(
+                onClick = { money_sum += 9; Toast.makeText(context, "Pizza Added!", Toast.LENGTH_SHORT).show() }
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.add_icon),
+                    contentDescription = "Add item",
+                    Modifier.size(20.dp)
+                )
+                Text(text = "Add")
+            }
+        }
+        Row(modifier = Modifier.height(intrinsicSize = IntrinsicSize.Max)) {
+            Text(
+                text = "The Works Pizza 8$",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(10.dp)
+            )
+            Button(
+                onClick = { money_sum += 8; Toast.makeText(context, "Pizza Added!", Toast.LENGTH_SHORT).show() }
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.add_icon),
